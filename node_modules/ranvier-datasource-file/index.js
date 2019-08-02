258370c0fdfd14c90759092c76bbb5292359dd6a
StickiStickman <stickman.player@gmail.com>
2019-08-02 20:02:36 +0200

    Removed modules, added auto ci for bundles

--- node_modules/ranvier-datasource-file/index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-module.exports = {
-  JsonDataSource: require('./JsonDataSource'),
-  JsonDirectoryDataSource: require('./JsonDirectoryDataSource'),
-  YamlDataSource: require('./YamlDataSource'),
-  YamlAreaDataSource: require('./YamlAreaDataSource'),
-  YamlDirectoryDataSource: require('./YamlDirectoryDataSource'),
-};
